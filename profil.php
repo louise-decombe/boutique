b1a43b271c138ce4bcfa049a42520a4cead2f261
nadia-rais <nadia.rais@laplateforme.io>
2020-08-06 09:44:52 +0200

    profile fichier en cours

--- profil.php
+++ profil.php
@@ -13,12 +13,50 @@
 </head>
 
 <body>
-    <header>
+<header>
         <?php 
         include("includes/header.php");
+        //$infos = $user->profile($_SESSION['user']['id_user']);
+        //var_dump($infos);
+        var_dump($_SESSION['user']);
+
+        if (isset($_POST[""])) {
+            $user->update_infos();
+        }
         ?>
     </header>
     <main>
+        <section id="container-profile">
+            <article id="profile-infos">
+                <h2 id="profile-title">Mes informations<h2>
+                <p><?= $_SESSION['user']['firstname'] ?>&nbsp;<?= $_SESSION['user']['lastname']?></p>
+                <address><p>Tel : <?= $_SESSION['user']['phone'] ?></p></address>
+                
+                <form id='onglet' method='POST'>
+                    <input id="link-modif" name="modifier" value="modifier" type="submit"/>
+                </form>
+            </article>
+            <?php if(isset($_POST['modifier'])){?>
+                    <section>
+                        <h3>MODIFIER MES INFOS</h3>
+                        <form action="profil.php" method='POST'>
+                            <section id="box-gender">
+                                <label>CIVILITÉ</label>
+                                <input type="radio" name="gender" id="female" value="Femme">
+                                <label for="female">madame</label>
+                                <input type="radio" name="gender" id="male" value="Homme">
+                                <label for="male">monsieur</label>
+                                <input type="radio" name="gender" id="no_gender" value="Non genré">
+                                <label for="no_gender">non genré</label>
+                            </section>
+                            <input type="text" name="firstname" placeholder="prénom*">
+                            <input type="text" name="lastname" placeholder="nom*">
+                            <input type="tel" name="phone" placeholder="0123456789*">
+                            <button type="submit" name="submit">MODIFIER</button>
+                        </form>
+                    </section>
+               <?php } ?>
+        </section>
     </main>
     <footer>
         <?php include('includes/footer.php'); ?>
