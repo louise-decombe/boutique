e8e67f2a0e7b6dae235d80978dba11e1a049976f
nadia-rais <nadia.rais@laplateforme.io>
2020-09-23 16:33:54 +0200

    update admin articles

--- admin_articles.php
+++ admin_articles.php
@@ -1,12 +1,4 @@
-<?php $page_selected = 'admin_articles.php'; ?>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-<?php
-include("includes/header.php");
-require('admin_nav.php');
-
-?>
+<?php $page_selected = 'admin_articles'; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,16 +8,34 @@ require('admin_nav.php');
     <link rel="shortcut icon" type="image/x-icon" href="https://i.ibb.co/0mKd0xT/icon-round-fanzine.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
           integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<<<<<<< Updated upstream
           <link rel="stylesheet" href="css/admin-nad.css">
           <link rel="stylesheet" href="css/admin.css">
           <link rel="stylesheet" type="text/css" href="css/style-item.css">
 
 
+=======
+    <link rel="stylesheet" href="css/style-admin-general.css">
+    <link rel="stylesheet" href="css/admin.css">
+>>>>>>> Stashed changes
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 </head>
 <body>
+  <header>
+  <?php
+  include("includes/header.php");
+  ?>
+</header>
+  <main>
+
+  <section id="nav-admin-pages">
+    <?php require("admin_nav.php"); ?>
+  </section>
+
 
   <?php
+<<<<<<< Updated upstream
 if ($user->is_admin==0) {
     ?>
     <center>
@@ -36,21 +46,30 @@ if ($user->is_admin==0) {
 <div class="valider">
   <a href="admin_articles.php?ajouter" id="more_infos">ajouter un article</a><br/>
 
+<<<<<<< Updated upstream
 </div>
 </div>
 </center>
+=======
+=======
+  if ($user->is_admin==0) { ?>
+  <a href="admin_articles.php?articles">Voir les articles</a><br/>
+  <a href="admin_articles.php?ajouter">ajouter un article</a><br/>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   <?php
-
   if (isset($_GET['articles'])) {
       if (!empty($_SESSION['statusMsg'])) {
           echo '<p>'.$_SESSION['statusMsg'].'</p>';
           unset($_SESSION['statusMsg']);
-      } ?>
-      <div class="container-treatment">
+      } 
+  ?>
+  <div class="container-treatment">
     <div class="treatment-order">
           <div class="titre">
-<h3>ARTICLES </h3>
+            <h3>ARTICLES </h3>
           </div>
+<<<<<<< Updated upstream
           <div class="rtable">
 
             <table class="table">
@@ -86,9 +105,42 @@ if ($user->is_admin==0) {
                         <a href="admin_articles.php?id_article=<?php echo $user['id_article']; ?>" class="glyphicon glyphicon-edit"></a>
                         <a href="action_article.php?action_type=delete&id_article=<?php echo $user['id_article']; ?> " onclick="return confirm('Voulez vous vraiment supprimer cette entrée?');">X</a>
                     </td>
+=======
+          <table class="table">
+              <tr>
+                  <th >Nom du fanzine</th>
+                  <th >Auteur</th>
+                  <th >Editions</th>
+                  <th >Citation</th>
+                  <th >Nbe de pages</th>
+                  <th >Année de parution</th>
+                  <th >Prix</th>
+                  <th >Date d'ajout</th>
+                  <th></th>
+>>>>>>> Stashed changes
                 </tr>
-                <?php
-          }
+  <?php
+    $users = $db->getRows('article', array('order_by'=>'id_article DESC'));
+    if (!empty($users)) {
+      $count = 0;
+      foreach ($users as $user) {
+              $count++; 
+  ?>
+              <tr>
+                  <td><?php echo $user['nom_article']; ?></td>
+                  <td><?php echo $user['auteur_article']; ?></td>
+                  <td><?php echo $user['editions_article']; ?></td>
+                  <td><?php echo $user['citation_article']; ?></td>
+                  <td><?php echo $user['nb_pages']; ?></td>
+                  <td><?php echo $user['annee_parution']; ?></td>
+                  <td><?php echo $user['prix_article']; ?>   euros</td>
+                  <td><?php echo $user['date_registration']; ?></td>
+                  <td>
+                      <a href="admin_articles.php?id_article=<?php echo $user['id_article']; ?>"><i class="far fa-edit"></i></a>
+                      <a href="action_article.php?action_type=delete&id_article=<?php echo $user['id_article']; ?> " onclick="return confirm('Voulez vous vraiment supprimer cette entrée?');">X</a>
+                  </td>
+              </tr>
+  <?php }
       } else { ?>
                 <tr><td colspan="4">Aucun article trouvé......</td>
                 <?php }
@@ -307,10 +359,6 @@ if (isset($_GET['submit_form1'])) { ?>
     echo "vous n'avez pas le droit d'accéder à cette page, bien essayé ;)";
     echo "<a href='index.php'> Retour à l'accueil </a>";
 }   ?>
-  </main>
-  </body>
-  </html>
-
 </main>
 </body>
 </html>
