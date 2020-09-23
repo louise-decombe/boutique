a7ea812dd15b2114bf8293893411d6a034506fbf
Louise Décombe <62133595+louise-decombe@users.noreply.github.com>
2020-09-23 06:59:40 -0400

    Update admin_nav.php

--- admin_nav.php
+++ admin_nav.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php $page_selected = 'admin_nav.php';
 ?>
 
@@ -11,72 +10,7 @@
     <title></title>
   </head>
   <body>
-    <div class="admin">
 
-    <div class="container_nav_admin">
-  <ul>
-<div class="navbar">
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">COMMANDES
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-    <a href="admin-orders.php">  Commandes </a>
-    </div>
-  </div>
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">UTILISATEURS
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-      <a href="admin_utilisateurs.php?utilisateurs"> Tout les utilisateurs</a>
-      <a href="admin_utilisateurs.php?ajouter"> Ajouter un utilisateur</a>
-    </div>
-  </div>
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">CATEGORIES
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-      <a href="admin_categories.php?categories"> Catégories</a>
-      <a href="admin_categories.php?sous_categorie"> Sous-catégories</a>
-      <a href="admin_categories.php?ajouter"> Ajouter</a>
-    </div>
-  </div>
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">MESSAGES
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-      <a href="admin_messages.php?clients"> clients</a>
-      <a href="admin_messages.php?vendeurs"> vendeurs</a>
-    </div>
-  </div>
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">ARTICLES
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-      <a href="admin_articles.php?articles">Tout les produits</a>
-      <a href="admin_articles.php?ajouter">Ajouter un produit</a>
-    </div>
-  </div>
-
-  <div class="dropdown_nav">
-    <button class="dropbtn">NEWSLETTER
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content-nav">
-      <a href="admin_newsletter.php">Voir les inscrits</a>
-      <a href="envoi_newsletter.php?">Envoyer une newsletter</a>
-    </div>
-  </div>
-=======
 <div id="container-nav">
     <nav>
         <ul>
@@ -85,7 +19,7 @@
             <!-- First Tier Drop Down -->
             <ul>
                 <li><a href="admin-orders.php">tableau de bord</a></li>
-            </ul>        
+            </ul>
             </li>
             <li><a href="#">UTILISATEURS</a>
             <!-- First Tier Drop Down -->
@@ -124,7 +58,7 @@
             <ul>
                 <li><a href="admin_messages.php?clients">clients</a></li>
                 <li><a href="admin_messages.php?vendeurs">vendeurs</a></li>
-            
+
             	<!-- Second Tier Drop Down -->
             </ul>
             </li>
@@ -133,7 +67,7 @@
             <ul>
                 <li><a href="admin_messages.php?clients">voir les inscrits</a></li>
                 <li><a href="envoi_newsletter.php?">Envoyer une newsletter</a></li>
-            
+
             	<!-- Second Tier Drop Down -->
             </ul>
             </li>
@@ -146,5 +80,4 @@
             </li>
         </ul>
     </nav>
->>>>>>> Stashed changes
 </div>
