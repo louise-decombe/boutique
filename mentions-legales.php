0c54eb4fd82b9174d77a133383681774674275df
louise <louise.decombe@laplateforme.io>
2020-09-24 11:38:16 -0400

    up

--- mentions-legales.php
+++ mentions-legales.php
@@ -23,14 +23,15 @@
     </header>
     <main>
 
-      <div class="box">
 
-        <h1>Mentions légales</h1>
-        Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet <a href="http://hello@fanzinebookstore.com">hello@fanzinebookstore.com</a> sont :
-
-      </div>
 
       <div class="container">
+        <div class="box">
+
+          <h1>Mentions légales</h1>
+          Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet <a href="http://hello@fanzinebookstore.com">hello@fanzinebookstore.com</a> sont :
+
+        </div>
         <ul class="acc">
 
         <li>
